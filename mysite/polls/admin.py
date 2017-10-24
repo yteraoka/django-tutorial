a5826145cf4ac14578c4b2fc3758fe1be73157b6
Yoshinori Teraoka <teraoka.yoshinori@gmail.com>
2017-10-24 23:06:49 +0900

    admin page のさらなる改善

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -14,7 +14,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 
